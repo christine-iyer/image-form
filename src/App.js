5aee3299ffafee2e8c2522a70327179f0615828a
chrisIyer <hellohotbox420@gmail.com>
2023-05-03 19:35:06 -0400

    ok clean

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
+import { useState } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateEntry from './components/CreateEntry/CreateEntry';
 
 function App() {
+  const handleChange = (event) => {
+    setEntry({ ...entry, [event.target.name]: event.target.value })
+  }
+  const [entry, setEntry] = useState({
+    title: '',
+    category:'',
+    image: '', 
+    body: ''
+  })
+  const [entrys, setEntrys] = useState([])
+  const createEntry = async () => {
+    try {
+      const response = await fetch('/api/entrys', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ ...entry })
+      })
+      const data = await response.json()
+      setEntrys([data, ...entrys])
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setEntry({
+        title: '',
+        category:'',
+        image: '', 
+        body: ''
+      })
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-       <CreateEntry />
+      <CreateEntry
+        createEntry={createEntry}
+        entry={entry}
+        handleChange={handleChange}
+      />
       </header>
     </div>
   );
